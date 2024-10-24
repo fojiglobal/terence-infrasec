5306c90c1bf66ac146d38f4d290b228f64e6a31c
Terence <terenceawandem89@gmail.com>
2024-10-24 17:36:54 -0400

    Added working directory to qa

--- qa/us-east-2/gha-test.tf
+++ qa/us-east-2/gha-test.tf
@@ -2,11 +2,10 @@
 resource "aws_vpc" "prod" {
   cidr_block = "10.30.0.0/16"
   tags = {
-    Name = "dev-vpc"
-    Envronment = "dev"
+    Name       = "qa-vpc"
+    Envronment = "qa"
   }
 }
-
 terraform {
   required_providers {
     aws = {
@@ -14,8 +13,14 @@ terraform {
       version = "~> 5.0"
     }
   }
-}
 
+  backend "s3" {
+    bucket         = "terry-cs2-terraform"
+    key            = "qa/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "cs2-infrasec-terraform"
+  }
+}
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-2"
